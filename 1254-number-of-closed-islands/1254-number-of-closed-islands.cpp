c9d8724705cc12aa2a571e6d99e5441c7532fe74
Gyanendra Pal <125917025+gp02august@users.noreply.github.com>
2023-04-07 02:01:16 +0530

    Time: 17 ms (36.72%), Space: 9.4 MB (96.28%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,57 +1,62 @@
 class Solution {
 public:
 
-     bool isValid(int i, int j, int n, int m ,vector<vector<int>>& grid){
-        if(i>=0 && i<n && j>=0 && j<m && grid[i][j] == 0){
+       bool isValid(int i, int j, int row, int col, vector<vector<int>>& grid)
+    {
+        if(i>=0 && i<row && j>=0 && j<col && grid[i][j] == 0)
+        {
             return true;
         }
         return false;
     }
     
-    void dfs(int i, int j, int n, int m ,vector<vector<int>>& grid){
-        grid[i][j] = 1;
+    void dfs(int i, int j, int row, int col, vector<vector<int>>& grid)
+    {
+        grid[i][j]=1;
         
-        int ax[4] = {1, -1, 0, 0};
-        int ay[4] = {0, 0, 1, -1};
+        int ax[4]={1,-1, 0, 0};
+        int ay[4]={0, 0, 1, -1};
         
-        for(int k =0; k<4; k++){
-            int nx = i + ax[k];
-            int ny = j + ay[k];
-            
-            if(isValid(nx, ny, n, m, grid)){
-                dfs(nx, ny, n, m, grid);
+        for(int k=0; k<4; k++)
+        {
+            int nx=i+ax[k];
+            int ny=j+ay[k];
+            if(isValid(nx, ny, row, col, grid))
+            {
+                dfs(nx, ny, row, col, grid);
             }
         }
     }
-    
     int closedIsland(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        // boundary DFS
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(i*j == 0 || i == n-1 || j==m-1){
-                    // cout<<i<<" "<<j<<endl;
-                    if(grid[i][j] == 0){
-                        dfs(i, j, n, m, grid);
+        int row = grid.size();
+        int col = grid[0].size();
+    
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<col; j++)
+            {
+                if(i*j==0 || i==row-1 || j==col-1)
+                {
+                    if(grid[i][j] == 0)
+                    {
+                        dfs(i, j, row, col, grid);
                     }
                 }
             }
         }
         
-        // call DFS in whole grid
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j] == 0){
+        int ans=0;
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<col; j++)
+            {
+                if(grid[i][j] == 0)
+                {
                     ans++;
-                    dfs(i, j, n, m, grid);
+                    dfs(i, j, row, col, grid);
                 }
             }
         }
-        
         return ans;
     }
-    
 };
