29e97ba1a604374e729d405e89754a9bd4a52ff6
Gyanendra Pal <125917025+gp02august@users.noreply.github.com>
2023-04-07 23:39:52 +0530

    Time: 71 ms (86.45%), Space: 29.2 MB (62.13%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,80 +1,79 @@
 class Solution {
 public:
      
-     int count(vector<vector<int>>& grid){
-        int n = grid.size();
-        int m = grid[0].size();
-        int ans = 0;
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j] == 1)
+  int count(vector<vector<int>>& grid)
+    {
+        int row=grid.size();
+        int col=grid[0].size();
+        int ans=0;
+
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<col; j++)
+            {
+                if(grid[i][j]==1){
                     ans++;
+                }
             }
         }
-        
-        return ans;
+         return ans;
     }
     
-    bool isValid(int i, int j, int n, int m, vector<vector<int>>& grid){
-        if(i>=0 && i<n && j>=0 && j<m && grid[i][j] == 1 ){
+    bool isValid(int i, int j, int row, int col, vector<vector<int>>& grid)
+    {
+        if(i>=0 && i<row && j>=0 && j<col && grid[i][j]==1){
             return true;
         }
         return false;
     }
     
-    void dfs(int i, int j, int n, int m, vector<vector<int>>& grid){
-        grid[i][j] = 0;
+    void dfs(int i, int j, int row, int col, vector<vector<int>>& grid)
+    {
+        grid[i][j]=0;
         
         int ax[4] = {1, -1, 0, 0};
         int ay[4] = {0, 0, 1, -1};
         
-        for(int k=0; k<4; k++){
-            int nx = i + ax[k];
-            int ny = j + ay[k];
+        for(int k=0; k<4; k++)
+        {
+            int nx = i+ax[k];
+            int ny = j+ay[k];
             
-            if(isValid(nx,ny, n, m, grid)){
-                dfs(nx, ny, n, m, grid);
+            if(isValid(nx,ny, row, col, grid)){
+                dfs(nx, ny, row, col, grid);
             }
         }
     }
     
     int numEnclaves(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        int row = grid.size();
+        int col =grid[0].size();
         
-        
-        // boundary travers
-        for(int i=0; i<n; i++){
-            
-            int j = 0;
-            if(grid[i][j] == 1){
-                dfs(i, j, n, m, grid);
+        // boundry traversal;
+        for(int i=0; i<row; i++)
+        {
+            int j=0;
+            if(grid[i][j]==1)
+            {
+                dfs(i, j, row, col, grid);
+                
             }
-            
-            j = m-1;
-            if(grid[i][j] == 1){
-                dfs(i, j, n, m, grid);
+            j=col-1;
+            if(grid[i][j]==1)
+            {
+                dfs(i, j, row, col, grid);
             }
-            
         }
-        
-        
-        for(int j=0; j<m; j++){
-            
-            int i = 0;
-            if(grid[i][j] == 1){
-                dfs(i, j, n, m, grid);
-            }
-            
-            i = n-1;
-            if(grid[i][j] == 1){
-                dfs(i, j, n, m, grid);
-            }
-            
-        }
-        
+          for(int j=0; j<col; j++){
+              int i=0;
+              if(grid[i][j]==1){
+                  dfs(i, j, row, col, grid);
+              }
+                 i= row-1;
+                if(grid[i][j]==1){
+                    dfs(i, j, row, col, grid);
+                }
+          }
         return count(grid);
-        
     }
 };
