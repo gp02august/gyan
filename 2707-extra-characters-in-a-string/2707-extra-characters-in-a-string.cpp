4fa510d8174a6c806f487c33c6153ce43dbac834
Gyanendra Pal <125917025+gp02august@users.noreply.github.com>
2023-09-03 01:25:12 +0530

    Time: 95 ms (87.35%), Space: 62.5 MB (90.71%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+    /*
+    int dp[51];
+    int solve(string &s, unordered_set<string>st, int indx)
+    {
+        if(indx >= s.length())
+            return 0;
+        if(dp[indx] !=-1)
+            return dp[indx];
+        
+        string curr_str = "";
+        int minExt = INT_MAX;
+        
+        for(int i=indx; i<=s.length(); i++)
+        {
+            curr_str.push_back(s[i]);
+            int temp =0;
+            if(st.find(curr_str) == st.end())
+            {
+                temp = curr_str.length();
+            }
+            else
+            {
+                temp = 0;
+            }
+            int next_Ext = solve(s, st, i+1);
+            int totalExt = next_Ext + temp;
+            minExt = min(minExt, totalExt);
+        }
+        return dp[indx] = minExt;
+    }
+    int minExtraChar(string s, vector<string>& dictionary) {
+        unordered_set<string>st;
+        memset(dp, -1, sizeof(dp));
+        
+        for(int i=0; i<dictionary.size(); i++)
+        {
+            st.insert(dictionary[i]);
+        }
+        
+        return solve(s, st, 0);
+        */
+int dp[51];
+int solve(string &s, unordered_set<string> &st, int indx) {
+    if (indx >= s.length())
+        return 0;
+    if (dp[indx] != -1)
+        return dp[indx];
+
+    string curr_str = "";
+    int minExt = INT_MAX;
+
+    for (int i = indx; i < s.length(); i++) {
+        curr_str.push_back(s[i]);
+        int temp = 0;
+        if (st.find(curr_str) == st.end()) {
+            temp = curr_str.length();
+        } else {
+            temp = 0;
+        }
+        int next_Ext = solve(s, st, i + 1);
+        int totalExt = next_Ext + temp;
+        minExt = min(minExt, totalExt);
+    }
+    return dp[indx] = minExt;
+}
+
+int minExtraChar(string s, vector<string>& dictionary) {
+    unordered_set<string> st;
+    memset(dp, -1, sizeof(dp));
+
+    for (int i = 0; i < dictionary.size(); i++) {
+        st.insert(dictionary[i]);
+    }
+
+     return solve(s, st, 0);
+    } 
+    
+};
