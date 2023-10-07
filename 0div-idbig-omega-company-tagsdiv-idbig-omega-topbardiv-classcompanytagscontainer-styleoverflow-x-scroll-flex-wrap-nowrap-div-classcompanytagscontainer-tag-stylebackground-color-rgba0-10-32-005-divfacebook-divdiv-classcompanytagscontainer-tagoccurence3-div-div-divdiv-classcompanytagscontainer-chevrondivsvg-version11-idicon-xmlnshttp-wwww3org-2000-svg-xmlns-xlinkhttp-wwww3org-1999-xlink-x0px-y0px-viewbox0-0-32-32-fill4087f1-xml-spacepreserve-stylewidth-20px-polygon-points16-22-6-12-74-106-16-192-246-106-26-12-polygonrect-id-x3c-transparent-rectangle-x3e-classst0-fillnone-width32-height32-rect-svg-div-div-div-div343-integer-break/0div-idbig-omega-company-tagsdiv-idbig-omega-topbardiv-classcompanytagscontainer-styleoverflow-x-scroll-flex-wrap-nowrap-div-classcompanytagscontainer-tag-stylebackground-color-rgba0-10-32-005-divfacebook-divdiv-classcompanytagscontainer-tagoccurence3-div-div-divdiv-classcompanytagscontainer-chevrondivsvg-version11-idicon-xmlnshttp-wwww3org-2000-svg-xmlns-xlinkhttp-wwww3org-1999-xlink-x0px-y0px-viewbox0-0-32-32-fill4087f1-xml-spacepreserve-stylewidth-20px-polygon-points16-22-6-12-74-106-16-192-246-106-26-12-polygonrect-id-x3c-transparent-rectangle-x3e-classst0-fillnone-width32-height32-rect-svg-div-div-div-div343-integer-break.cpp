a2169581903575445dea03ded092c4189f1261cf
Gyanendra Pal <125917025+gp02august@users.noreply.github.com>
2023-10-08 01:53:00 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (83.39%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div343-integer-break/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div343-integer-break.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dp[59];
+    int solve(int n)
+    {
+        if(n==1)
+            return 1;
+        if(dp[n] != -1)
+            return dp[n];
+        int result = INT_MIN;
+        for(int i=1; i<n; i++)
+        {
+            int prod = i*max(n-i, solve(n-i));
+            result = max(result, prod);
+        }
+        return dp[n] = result;
+    }
+    int integerBreak(int n) {
+        memset(dp,-1, sizeof(dp));
+       return solve(n); 
+    }
+};
