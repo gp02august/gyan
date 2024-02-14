2a47445f34f295ab5864c7bfedb72a1f2cbf892a
Gyanendra Pal <125917025+gp02august@users.noreply.github.com>
2024-02-14 16:04:50 +0530

    Time: 151 ms (65.54%), Space: 143.1 MB (5.30%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2149-rearrange-array-elements-by-sign/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2149-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>v1;
+        vector<int>v2;
+        vector<int>res;
+        
+        int i=0;
+        while(i<n)
+        {
+            if(nums[i]>0)
+            {
+                v1.push_back(nums[i]);
+                i++;
+            }
+            else
+            {
+                v2.push_back(nums[i]);
+                i++;
+            }
+        }
+        
+        int j=0, k=0;
+        while(j<v1.size() && k<v2.size())
+        {
+            res.push_back(v1[j]);
+            res.push_back(v2[k]);
+            j++;
+            k++;
+        }
+        while(j<v1.size())
+        {
+            res.push_back(v1[j]);
+            j++;
+        }
+        while(k<v2.size())
+        {
+            res.push_back(v2[k]);
+            k++;
+        }
+        return res;
+    }
+};
