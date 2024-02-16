3c7c9f3a40d9d0a24c5adddeb6cc61c94ca7d3dd
Gyanendra Pal <125917025+gp02august@users.noreply.github.com>
2024-02-16 16:04:38 +0530

    Time: 101 ms (94.21%), Space: 66 MB (60.95%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divexpedia-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbookingcom-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsalesforce-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1481-least-number-of-unique-integers-after-k-removals/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divexpedia-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbookingcom-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsalesforce-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        
+        int n = arr.size();
+        unordered_map<int,int>mp;
+        for(auto i:arr)
+        {
+            mp[i]++;
+        }
+        vector<int>freq;
+        for(auto i:mp)
+        {
+            freq.push_back(i.second);
+        }
+        sort(freq.begin(), freq.end());
+        /*
+        for(size_t i=0; i<freq.size(); i++)
+        {
+            if(k)
+            {
+                freq.erase(freq.begin());
+                //cnt++;
+                k--;
+            }
+            
+            //int res=0;
+            //res = cnt+freq[]-m;
+        }
+        
+        
+        cout<<freq.size();
+        return freq.size();
+    }*/
+        int sum = 0;
+        int remainingUnique = 0;
+        
+        for (size_t i = 0; i < freq.size(); ++i) {
+            sum += freq[i];
+            
+            if (sum > k) {
+                remainingUnique = freq.size() - i;
+                break;
+            }
+        }
+        
+       
+        return remainingUnique;
+    }
+};
