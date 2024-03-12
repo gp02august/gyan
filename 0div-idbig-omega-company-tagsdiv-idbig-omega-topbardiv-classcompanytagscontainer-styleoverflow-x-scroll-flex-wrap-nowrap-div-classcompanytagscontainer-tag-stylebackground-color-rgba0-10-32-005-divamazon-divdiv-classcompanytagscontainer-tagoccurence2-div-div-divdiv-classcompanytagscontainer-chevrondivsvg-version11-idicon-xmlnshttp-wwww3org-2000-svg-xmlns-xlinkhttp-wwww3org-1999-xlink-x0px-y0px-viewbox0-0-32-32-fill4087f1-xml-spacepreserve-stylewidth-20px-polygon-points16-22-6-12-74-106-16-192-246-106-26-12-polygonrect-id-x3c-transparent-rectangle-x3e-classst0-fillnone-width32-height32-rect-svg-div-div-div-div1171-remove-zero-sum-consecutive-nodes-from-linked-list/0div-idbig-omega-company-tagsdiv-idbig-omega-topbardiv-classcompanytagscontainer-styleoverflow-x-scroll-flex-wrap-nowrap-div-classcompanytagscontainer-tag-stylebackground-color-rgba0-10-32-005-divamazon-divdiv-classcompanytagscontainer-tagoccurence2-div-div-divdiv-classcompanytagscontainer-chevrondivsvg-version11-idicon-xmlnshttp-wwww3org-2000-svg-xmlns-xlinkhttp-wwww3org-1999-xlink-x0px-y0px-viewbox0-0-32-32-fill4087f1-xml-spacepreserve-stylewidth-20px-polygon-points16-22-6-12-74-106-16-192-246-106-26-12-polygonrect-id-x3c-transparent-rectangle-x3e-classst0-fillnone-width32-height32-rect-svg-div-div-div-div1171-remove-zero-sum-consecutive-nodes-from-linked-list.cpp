be28ab3ddcaa8d862a437c05a5d86f97ca5c1e75
Gyanendra Pal <125917025+gp02august@users.noreply.github.com>
2024-03-13 00:09:34 +0530

    Time: 3 ms (97.32%), Space: 15 MB (16.76%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1171-remove-zero-sum-consecutive-nodes-from-linked-list/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1171-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        int prefixSum=0;
+        unordered_map<int,ListNode*>mp;
+        mp[0]=dummy;
+        while(head != NULL)
+        {
+            prefixSum +=head->val;
+            if(mp.find(prefixSum) != mp.end())
+            {
+                ListNode* start=mp[prefixSum];
+                ListNode* temp=start;
+                int crrntSm=prefixSum;
+                while(temp != head)
+                {
+                    temp=temp->next;
+                    crrntSm +=temp->val;
+                    if(temp != head)
+                        mp.erase(crrntSm);
+                }
+                start->next=head->next;
+            }
+            else
+            {
+                mp[prefixSum]=head;
+            }
+            head=head->next;
+        }
+        return dummy->next;
+    }
+};
