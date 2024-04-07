a36f049b3f5451f65f497e9d22ed3867fc027dd8
Gyanendra Pal <125917025+gp02august@users.noreply.github.com>
2024-04-08 00:44:49 +0530

    Time: 2 ms (47.10%), Space: 8.3 MB (12.09%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divyahoo-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div678-valid-parenthesis-string/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divyahoo-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div678-valid-parenthesis-string.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int t[101][101];
+    bool solve(int indx, int open, string &s, int n){
+        if(indx==n)
+            return open==0;
+        
+        if(t[indx][open] != -1)
+            return t[indx][open];
+        bool isValid = false;
+        if(s[indx]=='*'){
+            isValid |= solve(indx+1, open+1, s, n);
+            isValid |= solve(indx+1, open, s, n);
+            
+            if(open > 0){
+                isValid |= solve(indx+1, open-1, s, n);
+            }
+        }
+        else if(s[indx]=='('){
+            isValid |= solve(indx+1, open+1, s, n);
+        }
+        else if(open > 0){
+            isValid |= solve(indx+1, open-1, s, n);
+        }
+        return t[indx][open] = isValid;
+    }
+    bool checkValidString(string s) {
+        int n = s.length();
+        memset(t, -1, sizeof(t));
+        /*for(int i=0; i<n; i++){
+            if(s[i] == '(' || s[i] == ')' || s[i] == '*'){
+                continue;
+            }
+            else{
+                return false;
+                break;
+            }
+        }
+        return true;*/
+        
+        int indx=0, open=0;
+        return solve(indx, open, s, n);
+    }
+};
