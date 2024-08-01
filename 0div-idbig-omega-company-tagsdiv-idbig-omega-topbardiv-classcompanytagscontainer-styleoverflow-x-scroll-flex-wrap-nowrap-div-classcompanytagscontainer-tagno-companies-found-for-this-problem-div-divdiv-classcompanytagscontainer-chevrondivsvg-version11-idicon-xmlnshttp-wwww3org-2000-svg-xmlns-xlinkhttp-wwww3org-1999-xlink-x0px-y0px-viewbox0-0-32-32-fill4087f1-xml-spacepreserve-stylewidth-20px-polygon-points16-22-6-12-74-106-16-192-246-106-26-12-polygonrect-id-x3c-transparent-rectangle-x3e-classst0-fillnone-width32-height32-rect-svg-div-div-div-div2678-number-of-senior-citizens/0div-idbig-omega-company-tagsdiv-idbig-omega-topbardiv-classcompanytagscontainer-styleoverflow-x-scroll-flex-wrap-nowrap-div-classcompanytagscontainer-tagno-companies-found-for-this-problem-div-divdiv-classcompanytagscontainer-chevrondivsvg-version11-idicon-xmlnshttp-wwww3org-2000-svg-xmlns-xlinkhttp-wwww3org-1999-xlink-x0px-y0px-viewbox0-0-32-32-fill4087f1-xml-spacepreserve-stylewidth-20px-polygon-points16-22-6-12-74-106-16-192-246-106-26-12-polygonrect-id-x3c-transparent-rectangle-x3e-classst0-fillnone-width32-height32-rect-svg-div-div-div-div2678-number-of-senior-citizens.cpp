53b5a15df40504d24b23290a29ec9885cfb688ef
Gyanendra Pal <125917025+gp02august@users.noreply.github.com>
2024-08-01 15:50:00 +0530

    Time: 6 ms (60.14%), Space: 17.7 MB (16.71%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2678-number-of-senior-citizens/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2678-number-of-senior-citizens.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+          int count = 0;
+
+    for (const string& info : details) {
+        string ageStr = info.substr(11, 2);
+        int age = stoi(ageStr);
+        if (age > 60) {
+            count++;
+        }
+    }
+
+    return count;
+    }
+};
