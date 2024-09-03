af36834e80a66ae3a25e86137ec7064d06118588
Gyanendra Pal <125917025+gp02august@users.noreply.github.com>
2024-09-03 23:46:17 +0530

    Time: 7 ms (14.68%), Space: 11 MB (6.17%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1945-sum-of-digits-of-string-after-convert/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1945-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        unordered_map<char,int>mp;
+        mp['a']=1;
+        mp['b']=2;
+        mp['c']=3;
+        mp['d']=4;
+        mp['e']=5;
+        mp['f']=6;
+        mp['g']=7;
+        mp['h']=8;
+        mp['i']=9;
+        mp['j']=10;
+        mp['k']=11;
+        mp['l']=12;
+        mp['m']=13;
+        mp['n']=14;
+        mp['o']=15;
+        mp['p']=16;
+        mp['q']=17;
+        mp['r']=18;
+        mp['s']=19;
+        mp['t']=20;
+        mp['u']=21;
+        mp['v']=22;
+        mp['w']=23;
+        mp['x']=24;
+        mp['y']=25;
+        mp['z']=26;
+        
+//         unordered_map<char, int> mp;
+
+//         // Initialize the map with the positions of each alphabet character
+//         for (char ch = 'a'; ch <= 'z'; ++ch) {
+//             mp[ch] = ch - 'a' + 1;
+//         }
+
+        string res;
+        int sum = 0;
+
+        // Convert each character in the string s to its corresponding number
+        for (char ch : s) {
+            res += to_string(mp[ch]);
+        }
+
+        // Calculate the sum of digits after conversion
+        for (char c : res) {
+            int d = c - '0';  // Convert character to digit
+            sum += d;
+        }
+
+        // Perform the sum of digits operation k-1 times
+        for (int i = 1; i < k; ++i) {
+            int newSum = 0;
+            while (sum != 0) {
+                newSum += sum % 10;
+                sum /= 10;
+            }
+            sum = newSum;
+        }
+
+        return sum;
+    }
+};
